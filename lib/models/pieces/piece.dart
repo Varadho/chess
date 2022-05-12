0487639d2a36232165bab1f23bdd3c137ca3953b
Mateusz Czajkowski <mateusz.czajkowski@mni.thm.de>
2022-05-12 15:25:31 +0200

    enable linux desktop support

--- lib/models/pieces/piece.dart
+++ lib/models/pieces/piece.dart
@@ -20,5 +20,25 @@ abstract class Piece {
   }
 
   Widget figurine();
+
+  Widget _figurineInternal(String src) => Center(
+        child: Container(
+          height: 32,
+          width: 32,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: isWhite ? Color(0xFFFFFFE8) : Colors.black,
+          ),
+          child: Center(
+            child: Text(
+              src,
+              style: TextStyle(
+                color: isWhite ? Colors.black : Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+      );
   Piece({this.isWhite = true});
 }
